e01634aebde420a245d718adf7e982f646d6f099
Alex Ianus <hire@alexianus.com>
2015-02-20 14:45:59 -0500

    Show example of creating an order

--- app/src/main/java/com/coinbase/sdk/example/MainActivity.java
+++ app/src/main/java/com/coinbase/sdk/example/MainActivity.java
@@ -1,6 +1,5 @@
 package com.coinbase.sdk.example;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,10 +9,10 @@ import android.widget.TextView;
 import com.coinbase.android.sdk.OAuth;
 import com.coinbase.api.Coinbase;
 import com.coinbase.api.CoinbaseBuilder;
+import com.coinbase.api.entity.Button;
 import com.coinbase.api.entity.OAuthTokensResponse;
+import com.coinbase.api.entity.Order;
 import com.coinbase.api.exception.CoinbaseException;
-
-import java.util.concurrent.Semaphore;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
@@ -21,65 +20,37 @@ import roboguice.util.RoboAsyncTask;
 
 public class MainActivity extends RoboActivity {
 
-  private static final String CLIENT_ID = "5c9df3d9ac69af05fc03be0c25e8b75bc578c883f65cd3cb329201f29e29e5d5";
-  private static final String CLIENT_SECRET = "7676804342763d9be5a84296839db19a3117161df4ad22be3a838234a7bc9e51";
-  private static final String REDIRECT_URI = "coinbase-android-example://coinbase-oauth";
+  private static final String API_KEY = "YOUR_API_KEY";
+  private static final String API_SECRET = "YOUR_API_SECRET";
 
   @InjectView(R.id.the_text_view)
   private TextView mTextView;
 
-  public class DisplayEmailTask extends RoboAsyncTask<String> {
-    private OAuthTokensResponse mTokens;
+  public class CreateOrderTask extends RoboAsyncTask<Order> {
 
-    public DisplayEmailTask(OAuthTokensResponse tokens) {
+    public CreateOrderTask() {
       super(MainActivity.this);
-      mTokens = tokens;
     }
 
-    public String call() throws Exception {
-      Coinbase coinbase = new CoinbaseBuilder().withAccessToken(mTokens.getAccessToken()).build();
-      return coinbase.getUser().getEmail();
+    public Order call() throws Exception {
+      Coinbase coinbase = new CoinbaseBuilder().withApiKey(API_KEY, API_SECRET).build();
+
+      Button buttonParams = new Button();
+      buttonParams.setText("Pay with wallet");
+      buttonParams.setDescription("Pay with wallet");
+      buttonParams.setPrice(org.joda.money.Money.parse("USD 1.23"));
+      buttonParams.setName("Pay with wallet");
+      return coinbase.createOrder(buttonParams);
     }
 
     @Override
     public void onException(Exception ex) {
-      mTextView.setText("There was an error fetching the user's email address: " + ex.getMessage());
+      mTextView.setText("There was an error creating the order: " + ex.getMessage());
     }
 
     @Override
-    public void onSuccess(String email) {
-      mTextView.setText("Success! The user's email address is: " + email);
-    }
-  }
-
-  public class CompleteAuthorizationTask extends RoboAsyncTask<OAuthTokensResponse> {
-    private Intent mIntent;
-
-    public CompleteAuthorizationTask(Intent intent) {
-      super(MainActivity.this);
-      mIntent = intent;
-    }
-
-    @Override
-    public OAuthTokensResponse call() throws Exception {
-      return OAuth.completeAuthorization(MainActivity.this, CLIENT_ID, CLIENT_SECRET, mIntent.getData());
-    }
-
-    @Override
-    public void onSuccess(OAuthTokensResponse tokens) {
-      new DisplayEmailTask(tokens).execute();
-    }
-
-    @Override
-    public void onException(Exception ex) {
-      mTextView.setText("There was an error fetching access tokens using the auth code: " + ex.getMessage());
-    }
-  }
-
-  @Override
-  protected void onNewIntent(final Intent intent) {
-    if (intent != null && intent.getAction() != null && intent.getAction().equals("android.intent.action.VIEW")) {
-      new CompleteAuthorizationTask(intent).execute();
+    public void onSuccess(Order order) {
+      mTextView.setText("Success! The receive address is: " + order.getReceiveAddress());
     }
   }
 
@@ -88,11 +59,7 @@ public class MainActivity extends RoboActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    try {
-      OAuth.beginAuthorization(this, CLIENT_ID, "user", REDIRECT_URI, null);
-    } catch (CoinbaseException ex) {
-      mTextView.setText("There was an error redirecting to Coinbase: " + ex.getMessage());
-    }
+    new CreateOrderTask().execute();
   }
 
   @Override
